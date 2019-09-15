c78f1855d8830342f9ff4f3111512f76926d34c4
gshelly <glen.shelly@here.com>
2019-09-15 18:30:12 -0500

    update #2

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload, got it?
+          Edit <code>src/App.tsx</code> and save to reload this important file.
         </p>
         <a
           className="App-link"
